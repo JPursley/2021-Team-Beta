d47d5c73c8feff0ce27045e455ab255c14827754
Marco Villaseñor <marco@interastar.com>
2021-07-18 02:19:50 -0500

    Beta
    Webhook: getSession+getToken
    ActivityMaintExtension

--- CRActivityMaintExtension.cs
+++ CRActivityMaintExtension.cs
@@ -1,15 +1,32 @@
+using PX.Api;
+using PX.Data;
+using PX.Objects.CR;
+using PX.Objects.EP;
+using PX.SM;
+using PX.SmsProvider;
+using PX.SmsProvider.SM.DAC;
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using PX.SM;
-using PX.Data;
-using PX.Objects.EP;
-using PX.Objects.CR;
+using System.Data.Common;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Autofac;
+using Newtonsoft.Json;
+using PX.SmsProvider.SM;
 
 namespace TeamBeta2021
 {
     public class CRActivityMaintExtension : PXGraphExtension<CRActivityMaint>
     {
+        public PXSelect<SmsPluginParameter, Where<SmsPluginParameter.pluginName, Equal<Current<SmsPlugin.name>>>,
+            OrderBy<Asc<SmsPluginParameter.lineNumber>>> Details;
+
+        public PXSetup<TBetaCred> Creds;
+        [InjectDependency] public IReadOnlyDictionary<string, ISmsProviderFactory> ProviderFactories { get; set; }
+
+
         public PXAction<CRActivity> tBetaInitiate;
 
         [PXButton]
@@ -17,10 +34,51 @@ namespace TeamBeta2021
             MapViewRights = PXCacheRights.Select)]
         public virtual IEnumerable TBetaInitiate(PXAdapter adapter)
         {
-            throw new PXRedirectToUrlException("http://www.SPScommerce.com",PXBaseRedirectException.WindowMode.Same,string.Empty);
-            TBetaChat docentry = PXGraph.CreateInstance<TBetaChat>();
-            //docentry.Document.Current = docentry.Document.Search<SOInvoice.refNbr>(Invoices.Current.ARInvoiceNbr);
-            //throw new PXRedirectRequiredException(docentry, true, null);
+            Base.Actions.PressSave();
+            CRCase myCase =
+                PXSelect<CRCase, Where<CRCase.noteID, Equal<Required<CRCase.noteID>>>>.Select(Base,
+                    Base.CurrentActivity.Current.RefNoteID);
+
+
+            Contact mycontact =
+                PXSelect<Contact, Where<Contact.contactID, Equal<Required<Contact.contactID>>>>.Select(Base,
+                    myCase.ContactID ?? 0);
+            if (mycontact == null)
+                throw new PXException("No Contact!!!");
+
+            var request = new SendMessageRequest()
+            {
+                RecepientPhoneNbr = (mycontact.Phone1 == null) ? mycontact.Phone2 : mycontact.Phone1,
+                RecepientSMSMessage = "Hey there, click this http://www.spscommerce.com"
+            };
+            request.RecepientPhoneNbr = request.RecepientPhoneNbr.Replace("(", "");
+            request.RecepientPhoneNbr = request.RecepientPhoneNbr.Replace(")", "");
+            PXTrace.WriteInformation(mycontact.DisplayName + " " + request.RecepientPhoneNbr);
+
+            try
+            {
+                Guid guid = Guid.Parse("A18E155A-7CBF-E911-8173-DAB1842B9396");
+                var pluginParameters = SmsPluginMaint.Def[guid];
+                var plugin = ProviderFactories[pluginParameters.typeName].Create(pluginParameters.parameters);
+                plugin.SendMessageAsync(request, CancellationToken.None).Wait();
+            }
+            catch (Exception ex)
+            {
+                string strEx = ex.Message;
+            }
+
+            //PXDatabase.SelectDataFields<>();
+
+            string apikey = Creds.Current.Apikey;
+
+            var surl = "https://hackathon.acumatica.com/Beta/Frames/vnr.html?key=" + apikey + "&id=" +
+                Base.CurrentActivity.Current.NoteID.ToString();
+
+
+            throw new PXRedirectToUrlException(surl, PXBaseRedirectException.WindowMode.Same,
+                string.Empty);
+
         }
+
     }
 }
--- TBetaWebhookHandler.cs
+++ TBetaWebhookHandler.cs
@@ -2,32 +2,267 @@ using Newtonsoft.Json;
 using PX.Common;
 using PX.Data;
 using PX.Data.Webhooks;
+using PX.DbServices;
 using PX.Objects;
+using PX.Objects.CR;
 using PX.Objects.IN;
 using System;
-using System;
 using System.Collections;
-using System.Collections.Generic;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
 using System.Net.Http.Formatting;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Web;
 using System.Web.Http;
 using System.Web.Http.Results;
+using OpenTokSDK;
+using System.Net.Http.Headers;
+using System.Net;
+using System.Text;
+
+using Newtonsoft;
 
 namespace TeamBeta2021
 {
     public class TBetaWebhookHandler : IWebhookHandler
     {
+		/*
+		 *     (POST) {webhook_url}?role=[local/remote]&action=getToken 
+				(GET) {webhook_url}?role=[local/remote]&action=getSession&id=[id] 
+				(POST) {webhook_url}?role=[local/remote]&action=startRecording&id=[id] 
+				(POST) {webhook_url}?role=[local/remote]&action=stopRecording&id=[id] 
+		*/
+		
 		public async Task<System.Web.Http.IHttpActionResult> ProcessRequestAsync(
 		  HttpRequestMessage request, CancellationToken cancellationToken)
 		{
-            var ok = new OkResult(request);
+			var _queryParameters = HttpUtility.ParseQueryString(request.RequestUri.Query);
+			string action = _queryParameters.Get("action");
 
-			return ok;
+			switch (action)
+                { 
+				case "getToken":
+					return await getToken(request);
+			
+				case "getSession":
+					return await getSession(request);
+
+				case "startRecording":
+					return new OkResult(request); // startRecording(request);
+
+				case "stopRecording":
+					return new OkResult(request); //stopRecording(request);
+				default:
+					return new OkResult(request);
+			}
+
 		}
 
+		public HttpResponseMessage buildResponse(System.Net.HttpStatusCode status, string content)
+        {
+			var response = new HttpResponseMessage( status);
+			response.Content = new StringContent(content);
+			response.Content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+			return response;
+		}
+
+		public class JsonTextActionResult : IHttpActionResult
+		{
+
+			public HttpRequestMessage Request { get; }
+
+			public string JsonText { get; }
+
+			public JsonTextActionResult(HttpRequestMessage request, string jsonText)
+			{
+				Request = request;
+				JsonText = jsonText;
+			}
+
+			public Task<HttpResponseMessage> ExecuteAsync(CancellationToken cancellationToken)
+			{
+				return Task.FromResult(Execute());
+			}
+
+			public HttpResponseMessage Execute()
+			{
+				var response = this.Request.CreateResponse(HttpStatusCode.OK);
+				response.Content = new StringContent(JsonText, Encoding.UTF8, "application/json");
+				return response;
+			}
+		}
+
+		public class InvalidHttpActionResult : IHttpActionResult
+		{
+
+			private readonly string _message;
+			private readonly HttpStatusCode _statusCode;
+
+
+			public InvalidHttpActionResult(HttpStatusCode statusCode, string message)
+			{
+				_statusCode = statusCode;
+				_message = message;
+			}
+
+
+			public Task<HttpResponseMessage> ExecuteAsync(CancellationToken cancellationToken)
+
+			{
+
+				HttpResponseMessage response = new HttpResponseMessage(_statusCode)
+				{
+					Content = new StringContent(_message)
+				};
+
+				return Task.FromResult(response);
+			}
+		}
+
+		private OpenTok getOpenTok()
+		{
+			using (var scope = GetUserScope())
+			{
+				var graph = PXGraph.CreateInstance<TBetaCredSetup>();
+				TBetaCred creds = graph.Creds.Select();
+				return new OpenTok(Convert.ToInt32(creds.Apikey), creds.VSecret);
+			}
+		}
+
+		private void saveSessionId(string id, string sessionId)
+		{
+			using (var scope = GetUserScope())
+			{
+				Guid noteID = Guid.Parse(id);
+
+				PXDatabase.Update<CRActivity>(
+					new PXDataFieldAssign("SessionID", sessionId),
+					new PXDataFieldRestrict("NoteID", PXDbType.UniqueIdentifier, 6, noteID, PXComp.EQ)
+					);
+			}
+		}
+
+		private string getSessionId(string id)
+		{
+			using (var scope = GetUserScope())
+			{
+				Guid noteID = Guid.Parse(id);
+
+				PXDataRecord rec = PXDatabase.SelectSingle<CRActivity>(
+					new PXDataField<TBetaCRActivityExtension.sessionID>(),
+					new PXDataFieldValue<CRActivity.noteID>(PXDbType.UniqueIdentifier, 6, noteID)
+					);
+
+				return rec.GetString(0);
+			}
+		}
+
+
+		public class TokenOptions
+        {
+			public string sessionID { get; set; }
+			public string role { get; set; }
+			public string data { get; set; }
+		}
+
+		private async Task<System.Web.Http.IHttpActionResult> getToken(HttpRequestMessage request)
+        {
+
+			var _queryParameters = HttpUtility.ParseQueryString(request.RequestUri.Query);
+			string client = _queryParameters.Get("role");
+
+			var body = await request.Content.ReadAsStringAsync();
+
+			var options = JsonConvert.DeserializeObject<TokenOptions>(body);
+
+			var openTok = getOpenTok();
+			var token = openTok.GenerateToken(options.sessionID, role: Role.PUBLISHER, data: options.data);
+
+			return new JsonTextActionResult(request, token); ;
+		}
+
+		private async Task<System.Web.Http.IHttpActionResult> getSession(HttpRequestMessage request)
+        {
+			var openTok = getOpenTok();
+
+			var _queryParameters = HttpUtility.ParseQueryString(request.RequestUri.Query);
+			string client = _queryParameters.Get("role");
+			string id = _queryParameters.Get("id");
+
+			string sessionId = null;
+
+			switch (client)
+            {
+				case "local":
+					// Create session, turn on automatic recording
+					var session = openTok.CreateSession(mediaMode: MediaMode.ROUTED, archiveMode: ArchiveMode.ALWAYS);
+					sessionId = session.Id;
+
+					saveSessionId(id, sessionId);
+					break;
+
+				case "remote":
+					sessionId = getSessionId(id);
+					break;
+			}
+
+			return new JsonTextActionResult(request, sessionId);
+		}
+
+		//private string StartRecording(HttpRequestMessage request)
+  //      {
+		//	string noteId = System.Web.HttpContext.Current.Request.QueryString["id"];
+
+		//	// Load the sessionID
+		//	PXDataRecord rec = PXDatabase.SelectSingle<CRActivity>(
+		//				new PXDataField<TBetaCRActivityExtension.sessionID>(),
+		//				new PXDataFieldValue<CRActivity.noteID>(PXDbType.UniqueIdentifier, 6, id)
+		//				);
+
+		//	string sessionId = rec.GetString(0);
+
+		//	var openTokService = new OpenTokService();
+		//	var archive = openTokService.OpenTok.StartArchive(sessionId, "Archiving Video", true, true, OpenTokSDK.OutputMode.COMPOSED);
+
+		//	return archive.Id.ToString();
+		//}
+
+		//private string StopRecording(HttpRequestMessage request)
+  //      {
+
+		//	string noteId = System.Web.HttpContext.Current.Request.QueryString["id"];
+		//	var openTokService = new OpenTokService();
+		//	var archive = openTokService.OpenTok.StopArchive(noteId);
+
+		//	return archive.Id.ToString();
+
+		//}
+
+
+		/// <summary>
+		/// Defines the LoginScope to be used for the WebHooks
+		/// </summary>
+		/// <returns></returns>
+		private IDisposable GetUserScope()
+		{
+			//todo: For now we will use admin but we will want to throttle back to a 
+			//      user with restricted access as to reduce any risk of attack.
+			//      perhaps this can be configured in the Surveys Preferences/Setup page.
+			var userName = "admin";
+			if (PXDatabase.Companies.Length > 0)
+			{
+				var company = PXAccess.GetCompanyName();
+				if (string.IsNullOrEmpty(company))
+				{
+					company = PXDatabase.Companies[0];
+				}
+				userName = userName + "@" + company;
+			}
+			return new PXLoginScope(userName);
+		}
 	}
 }
+
+
